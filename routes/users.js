2c4149d2f9774dfe6f0a45dab52e2fd25f271d10
MessaIina <121053491+MessaIina@users.noreply.github.com>
2023-09-19 18:35:46 +0700

    1REW 14PR

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const { REG_EXP_LINK } = require('../utils/constants');
+const { REG_EXP_EMAIL, REG_EXP_LINK } = require('../utils/constants');
 const {
   getProfileUser,
   getUsers,
@@ -41,7 +41,28 @@ router.patch(
   }),
   updateAvatar,
 );
-router.post('/signin', login);
-router.post('/signup', createUser);
+router.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(REG_EXP_LINK),
+      email: Joi.string().required().pattern(REG_EXP_EMAIL),
+      password: Joi.string().required().min(6),
+    }),
+  }),
+  createUser,
+);
+router.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().pattern(REG_EXP_EMAIL),
+      password: Joi.string().required().min(6),
+    }),
+  }),
+  login,
+);
 
 module.exports = router;
