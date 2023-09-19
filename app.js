cc2ec962c486ca8cdb95c092fc6ccf30c9a898c5
MessaIina <121053491+MessaIina@users.noreply.github.com>
2023-09-19 23:23:44 +0700

    1REW 14PR

--- app.js
+++ app.js
@@ -58,7 +58,8 @@ app.use('*', auth, (req, res, next) => {
 });
 
 app.use(errors());
-app.use((err, req, res) => {
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
   const { statusCode = INTERNAL_SERVER_ERROR, message } = err;
   res.status(statusCode).send({
     message:
