b2aa4626c8a369aa41b192bdd4d7c6a5105ed0e5
MessaIina <121053491+MessaIina@users.noreply.github.com>
2023-09-19 23:45:34 +0700

    1REW 14PR

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,7 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   if (!token) {
-    throw new UnauthorizedError('Неверный логин и/или пароль');
+    throw new UnauthorizedError('Необходима авторизация');
   }
   let payload;
   try {
