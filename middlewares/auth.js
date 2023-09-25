89fa324935bb00874780a023d86df1e8258bbdaf
MessaIina <121053491+MessaIina@users.noreply.github.com>
2023-09-26 00:01:18 +0700

    1

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,23 +1,26 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-error');
-const { NODE_ENV, JWT_SECRET } = require('../utils/app.config');
+const { SECRET_KEY } = require('../utils/constants');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Необходима авторизация');
+  const token = req.cookies.jwt;
+  if (!token) {
+    next(new UnauthorizedError('Необходима авторизация'));
+    return;
   }
-  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(
       token,
-      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+      NODE_ENV === 'production' ? JWT_SECRET : SECRET_KEY,
     );
   } catch (err) {
-    throw new UnauthorizedError('Неавторизован');
+    next(new UnauthorizedError('Неверный логин и/или пароль'));
+    return;
   }
-  req.user = payload; // записываем пейлоуд в объект запроса
-  next(); // пропускаем запрос дальше
+  req.user = payload;
+  next();
 };
